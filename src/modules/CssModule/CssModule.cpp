b8c742f41940c18b40295463a78440fc5e38e9dc
java2 <java2@BAKALEIA>
2025-09-21 12:27:33 +0600

    making css parsing

--- src/modules/CssModule/CssModule.cpp
+++ src/modules/CssModule/CssModule.cpp
@@ -5,63 +5,125 @@
 #include <regex>
 
 
-
-CssModule::CssModule()
+struct cssDeclarationBlockStruct
 {
+	std::string property;
+	std::string value;
+};
 
-}
-
-
-static std::string removeComments(std::string cssContent)
+struct cssRuleStruct
 {
-	std::regex commentRegex(R"(\/\*[^*]*\*+([^/*][^*]*\*+)*\/)");
-	return std::regex_replace(cssContent, commentRegex, "");
-}
+	std::string selector;
+	std::vector<cssDeclarationBlockStruct> declaration;
+};
+
+static std::string removeCssComments(std::string cssContent);
+static std::string readFile(std::string filePath);
+static std::string removeWhiteblanks(std::string inputString);
+static std::vector<cssRuleStruct> separateCssRules(std::string inputString);
+static void applyCss(std::vector<cssRuleStruct> cssRules, GuiStorageModule rootGui);
 
 
-static void localParseTagStyles(GuiStorageModule rootGui, std::string cssContent)
-{
 
-	size_t startPos = 0;
-	size_t endPos = 0;
-	while (cssContent.size() != 0)
-	{
-		startPos = cssContent.find_first_of('{');
-		endPos = cssContent.find_first_of('}');
-		std::string tag = cssContent.substr(0, startPos);
-		std::string attributes = cssContent.substr(startPos + 1, endPos - startPos - 1);
-		std::cout << "Tag: <" << tag << "> Body: <" << attributes << ">" << std::endl;
-		cssContent.replace(0, endPos + 1, "");
-	}
-}
+
+
 
 
 
 void CssModule::parseCss(GuiStorageModule rootGui, std::string filePath)
 {
-	std::string cssFileContent;
+	std::string cssFileContent = readFile(filePath);
+	cssFileContent = removeCssComments(cssFileContent);
+	cssFileContent = removeWhiteblanks(cssFileContent);
+	std::vector<cssRuleStruct> cssRules = separateCssRules(cssFileContent);
+	std::cout << "Start:" << std::endl;
+	for (size_t i = 0; i < cssRules.size(); i++)
+	{
+		std::cout << "_____________________________________" << std::endl;
+		std::cout << "Tag: " << cssRules.at(0).selector << std::endl;
+		std::cout << "Declaration:" << std::endl;
+		for (size_t j = 0; j < cssRules.at(i).declaration.size(); j++)
+		{
+			std::cout << "Property: <" << cssRules.at(i).declaration.at(j).property << "> Value: <" << cssRules.at(i).declaration.at(j).value << ">" << std::endl;
+		}
+	}
+}
+
+
+
+
+static std::string removeCssComments(std::string cssContent)
+{
+	std::regex commentRegex(R"(\/\*[^*]*\*+([^/*][^*]*\*+)*\/)");
+	return std::regex_replace(cssContent, commentRegex, "");
+}
+static std::string readFile(std::string filePath)
+{
+	std::string result = "";
 	std::ifstream fp(filePath);
+
 	if (!fp.is_open())
 	{
-		std::cout << "Css file: " << filePath << ", error open." << std::endl;
-		return;
+		std::cout << "Error open file: " << filePath << std::endl;
+		return result;
 	}
 	char delim = '\0';
-	std::getline(fp, cssFileContent, delim);
+	std::getline(fp, result, delim);
 	fp.close();
+	return result;
+}
+static std::string removeWhiteblanks(std::string inputString)
+{
+	std::string result = "";
+	for (char c : inputString)
+	{
+		if (c != ' ' && c != '\n')
+		{
+			result += c;
+		}
+	}
+	return result;
+}
+static std::vector<cssDeclarationBlockStruct> separateDeclarationBlock(std::string declarationBlockText)
+{
+	std::vector<cssDeclarationBlockStruct> result;
+	size_t startPos = 0;
+	size_t endPos = 0;
+	while (declarationBlockText.size() != 0)
+	{
+		cssDeclarationBlockStruct cssDeclarationBuffer;
+		startPos = declarationBlockText.find_first_of(':');
+		endPos = declarationBlockText.find_first_of(';');
+		std::string property = declarationBlockText.substr(0, startPos);
+		std::string value = declarationBlockText.substr(startPos + 1, endPos - startPos - 1);
+		cssDeclarationBuffer.property = property;
+		cssDeclarationBuffer.value = value;
+		result.push_back(cssDeclarationBuffer);
+		declarationBlockText.replace(0, endPos + 1, "");
+	}
+	return result;
+}
+static std::vector<cssRuleStruct> separateCssRules(std::string inputString)
+{
+	std::vector<cssRuleStruct> resultRules;
+	size_t startPos = 0;
+	size_t endPos = 0;
+	while (inputString.size() != 0)
+	{
 
-	std::string cssFileContentBufferErease;
-	for (char c : cssFileContent) if (c != ' ' && c != '\n') cssFileContentBufferErease += c;
-	cssFileContent = cssFileContentBufferErease;
-	if (cssFileContent.size() == 0 || cssFileContent.find('{') == std::string::npos || cssFileContent.find('}') == std::string::npos)
-	{
-		std::cout << "CSS file: " << filePath << " is empty or do not have { } body." << std::endl;
-		return;
+		startPos = inputString.find_first_of('{');
+		endPos = inputString.find_first_of('}');
+		std::string selector = inputString.substr(0, startPos);
+		std::vector<cssDeclarationBlockStruct> declarationBlock = separateDeclarationBlock(inputString.substr(startPos + 1, endPos - startPos - 1));
+		cssRuleStruct cssRuleBuffer;
+		cssRuleBuffer.selector = selector;
+		cssRuleBuffer.declaration = declarationBlock;
+		resultRules.push_back(cssRuleBuffer);
+		inputString.replace(0, endPos + 1, "");
 	}
-	else
-	{
-		cssFileContent = removeComments(cssFileContent);
-		localParseTagStyles(rootGui, cssFileContent);
-	}
-	
+	return resultRules;
+}
+static void applyCss(std::vector<cssRuleStruct> cssRules, GuiStorageModule rootGui)
+{
+
 }
--- src/modules/CssModule/CssModule.h
+++ src/modules/CssModule/CssModule.h
@@ -9,45 +9,5 @@
 class CssModule
 {
 public:
-	CssModule();
 	static void parseCss(GuiStorageModule rootGui, std::string filePath);
 };
-
-// css sheet
-
-/*
-
-Font{
-	font-size: 14px;
-	visibility: true/false;
-	color: #000000;
-	font-family: "путь до шрифта.ttf";
-	padding-left: 0px;
-	padding-right: 0px;
-	padding-bottom: 0px;
-	padding-top: 0px;
-	padding: 0px 0px 0px 0px; top right bottom left
-	opacity: 1.0 - 0.0;
-	order: 1, 2, 3...;
-}
-Image{
-	width: 100px;
-	height: 100px;
-	padding-left:0px;
-	padding-right: 0px;
-	padding-top: 0px;
-	padding-bottom: 0px;
-	visibility: true/false;
-	opacity: 1.0 - 0.0f;
-	order: 1, 2, 3...;
-}
-Div
-{
-	display: flex / block;
-	flex-direction: row / column / row-reverse / column - reverse;
-	flex-wrap: nowrap / wrap;
-	justify-content: start / end / center / space-between / space-around / space-evenly;
-	align-items: stretch / flex-start / flex-end / center;
-}
-
-*/
